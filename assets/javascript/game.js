d0bb260bc2c01caaf492d0905f1c912779c8c587
Christ <christ@Christs-MacBook-Pro.local>
2016-11-06 18:17:07 -0600

    html done, pseudocoded

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+// randomly generated number
+// each crystal (4) represents a number that is hidden (has a value)
+// each time you click it, it adds to your total score
+// the only way to see the value of the crystal is by clicking it
+// match your score to the random number
+// when the match happens, you get a win point
+// when you lose you get a minus point
+// the game restarts and the target number and the crystal value numbers change
+// random number to match is between 19 and 120
+// crystal numbers are between 1 and 12
+
+
+// create variables
+var crystal1 = ?;
+var crystal2 = ?;
+var crystal3 = ?;
+var crystal4 = ?;
+var targetNumber = ?;
+var userNumber = ?;
+var wins = 0;
+var losses = 0;
+
+// generate random number for targetNumber between 19 and 120
+
+// generate random number for crystal1 between 1 and 12
+// generate random number for crystal2 between 1 and 12
+// generate random number for crystal3 between 1 and 12
+// generate random number for crystal4 between 1 and 12
+// attach those values to the variables between 1 and 12
+
+
